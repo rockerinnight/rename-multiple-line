e3bc900c8d42f19dfb7eb2471f65b88d33d3ade4
Linh Doan <doanvulinh3108@gmail.com>
2024-04-03 00:44:03 +0700

    - Add Vercel Speed Insights module

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
 const vercelAnalytics = require("./node_modules/@vercel/analytics/dist/index.js");
+const vercelSpeedInsights = require("./node_modules/@vercel/speed-insights/dist//index.js");
 if (vercelAnalytics) vercelAnalytics.inject();
+if (vercelSpeedInsights) vercelSpeedInsights.injectSpeedInsights();
